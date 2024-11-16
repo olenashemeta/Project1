0d288aa8ba2b5406d093137cc4073fb63eef1c2b
slopachak <slopachak@stud.khpi.ucode-connect.study>
2024-11-16 11:40:56 +0200

    new submit account

--- log_page/src/on_submit_account_button_clicked.c
+++ log_page/src/on_submit_account_button_clicked.c
@@ -15,6 +15,32 @@ void on_submit_account_button_clicked(GtkWidget *button, gpointer data) {
     const char *login = gtk_entry_get_text(GTK_ENTRY(login_entry));
     const char *new_password = gtk_entry_get_text(GTK_ENTRY(new_password_entry));
     const char *confirm_password = gtk_entry_get_text(GTK_ENTRY(confirm_password_entry));
+    //fill all gap
+    if (mx_strcmp(new_username, "") == 0 || mx_strcmp(new_password, "") == 0 || mx_strcmp(login, "") == 0) {
+        gtk_label_set_text(GTK_LABEL(error_label), "Fill in all fields.");
+        return;
+    }
+    //log 1000
+    if (mx_strlen(login) > max_size) {
+        gtk_label_set_text(GTK_LABEL(error_label), "Login must be\n\tless than 1000 characters.");
+        return;
+    }else {
+        gtk_label_set_text(GTK_LABEL(error_label), ""); 
+    }
+    // pas 8
+    if (mx_strlen(new_password) < 8) {
+        gtk_label_set_text(GTK_LABEL(error_label), "Password must be\n\tmore than 8 characters.");
+        return;
+    }else {
+        gtk_label_set_text(GTK_LABEL(error_label), ""); 
+    }
+    // pas 1000
+    if (mx_strlen(new_password) > max_size) {
+        gtk_label_set_text(GTK_LABEL(error_label), "Password must be\n\tless than 1000 characters.");
+        return;
+    }else {
+        gtk_label_set_text(GTK_LABEL(error_label), ""); 
+    }
 
     if (mx_strcmp(new_password, confirm_password) != 0) {
         gtk_label_set_text(GTK_LABEL(error_label), "Passwords don't match!");
@@ -23,28 +49,7 @@ void on_submit_account_button_clicked(GtkWidget *button, gpointer data) {
         gtk_label_set_text(GTK_LABEL(error_label), ""); 
     }
 
-    if (mx_strlen(login) > max_size) {
-        gtk_label_set_text(GTK_LABEL(error_label), "Login must be\n\tless than 1000 characters.");
-        return;
-    }
-	
-    if (mx_strlen(new_password) > max_size) {
-        gtk_label_set_text(GTK_LABEL(error_label), "Password must be\n\tless than 1000 characters.");
-        return;
-    }
-
-	if (mx_strlen(new_password) < 8) {
-        gtk_label_set_text(GTK_LABEL(error_label), "Password must be\n\tmore than 8 characters.");
-        return;
-    }
-	
-    if (mx_strcmp(new_username, "") == 0 || mx_strcmp(new_password, "") == 0 || mx_strcmp(login, "") == 0) {
-        gtk_label_set_text(GTK_LABEL(error_label), "Fill in all fields.");
-        return;
-    }
-   
-
-	t_user* user = mx_create_user(login, new_username, new_password);
+   	t_user* user = mx_create_user(login, new_username, new_password);
 	
     g_print("Account created successfully:\n");
     g_print("Username: %s\n", user->name);
