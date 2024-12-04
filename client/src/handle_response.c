8e7515873cde4dae3a2110a3d9e2bb9dd324476b
Rostislav Bryhynets <rbryhynets@stud.khpi.ucode-connect.study>
2024-12-04 22:01:05 +0200

    hihi haha fixed response

--- client/src/handle_response.c
+++ client/src/handle_response.c
@@ -11,16 +11,56 @@ void handle_login_response(cJSON *json_payload) {
         printf("Login was successful\n");
 
         cJSON *data = cJSON_GetObjectItemCaseSensitive(json_payload, "data");
-        t_bee_user *user = create_bee_user_from_json(data);
-        if (!user) {
-            fprintf(stderr, "Failed to create user from JSON\n");
+        if (!data) {
+            fprintf(stderr, "Missing 'data' in JSON response\n");
             return;
         }
 
-        printf("User login: %s, username: %s, created at: %s, logo_id: %d\n",
-               user->login, user->username, user->created_at, user->logo_id);
+        t_bee_user user;
+        memset(&user, 0, sizeof(t_bee_user));
 
-        free_bee_user(user);
+        cJSON *login = cJSON_GetObjectItemCaseSensitive(data, "login");
+        cJSON *username = cJSON_GetObjectItemCaseSensitive(data, "username");
+        cJSON *created_at = cJSON_GetObjectItemCaseSensitive(data, "created_at");
+        cJSON *logo_id = cJSON_GetObjectItemCaseSensitive(data, "logo_id");
+
+        if (cJSON_IsString(login)) {
+            size_t decoded_len;
+            unsigned char *decoded = base64_decode(login->valuestring, &decoded_len);
+            if (decoded) {
+                user.login = strndup((char *)decoded, decoded_len);
+                free(decoded);
+            }
+        }
+
+        if (cJSON_IsString(username)) {
+            size_t decoded_len;
+            unsigned char *decoded = base64_decode(username->valuestring, &decoded_len);
+            if (decoded) {
+                user.username = strndup((char *)decoded, decoded_len);
+                free(decoded);
+            }
+        }
+
+        if (cJSON_IsString(created_at)) {
+            size_t decoded_len;
+            unsigned char *decoded = base64_decode(created_at->valuestring, &decoded_len);
+            if (decoded) {
+                user.created_at = strndup((char *)decoded, decoded_len);
+                free(decoded);
+            }
+        }
+
+        if (cJSON_IsNumber(logo_id)) {
+            user.logo_id = logo_id->valueint;
+        }
+
+        printf("User login: %s, username: %s, created at: %s, logo_id: %d\n",
+               user.login, user.username, user.created_at, user.logo_id);
+
+        free(user.login);
+        free(user.username);
+        free(user.created_at);
 
     } else {
         printf("Login failed\n");
